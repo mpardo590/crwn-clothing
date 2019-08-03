22a2eff6d33fa1193b037e88a7906d9c5b9b7ef4
mpardo590 <mpardo590@gmail.com>
2019-08-03 17:45:02 -0400

    added cartIcon and cartDropdown components to our application, also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
